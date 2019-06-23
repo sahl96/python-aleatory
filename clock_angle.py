5695b08a533ba71fe536b2c7c81bc3f072afa35c
Sahl Abdelsayed <50756102+sahl96@users.noreply.github.com>
2019-06-23 22:29:31 +0200

    Add files via upload

---
+++ clock_angle.py
@@ -0,0 +1,20 @@
+""" This program can caluclate the smaller angle between he the hour and minute arms of a clock """
+
+hour = int(input("Enter the hour (0-11): "))
+minute = int(input("Enter the minute (0-59): "))
+
+def hour_to_angle(hour):
+    return (hour/12) * 360
+
+def minute_to_angle(minute):
+    return (minute/60) *360
+
+def fraction_angle(minute):
+    return (minute/60) * 30
+
+angle_clock =   abs(hour_to_angle(hour) + fraction_angle(minute) - minute_to_angle(minute))
+
+if angle_clock >180:
+    angle_clock = 360 - angle_clock
+
+print("The smaller angle between the clock arms is ", angle_clock, "degrees")
